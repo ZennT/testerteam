30824e3ecd3a1a7d4d137c42811731b010337c01
Cihan Bulut <bulutcihan1970@gmail.com>
2022-02-25 16:50:47 -0500

    changing

--- src/test/java/Class01.java
+++ src/test/java/Class01.java
@@ -2,6 +2,6 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Class01 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("merhaba");
     }
 }
